4157570b03540ef6bfa2e8012108ab7e12b9a75f
Janshai <jack.r.s.adams@gmail.com>
2018-07-13 19:25:43 +0100

    Added Collectionview and cardcell

--- SmartBusinessCard/ViewController.swift
+++ SmartBusinessCard/ViewController.swift
@@ -8,18 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
 
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        collectionView.alwaysBounceVertical = true
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cardCell", for: indexPath) as? CardCell
+        cell?.setupViews()
+        return cell ?? UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 100.0)
+    }
+    
 
+}
 
+class CardCell: UICollectionViewCell {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var messagePreviewLabel: UILabel!
+    @IBOutlet weak var dateOfLastMessageLabel: UILabel!
+    
+    func setupViews() {
+        profileImageView.image = UIImage(named: "Zuck")
+        profileImageView.contentMode = .scaleAspectFill
+        profileImageView.translatesAutoresizingMaskIntoConstraints = false
+        profileImageView.layer.cornerRadius = 34
+        profileImageView.layer.masksToBounds = true
+        nameLabel.text = "Mark Zuckerberg"
+        messagePreviewLabel.text = "Mark: Hey there, cool app!"
+        dateOfLastMessageLabel.text = "9:05"
+    }
+    
 }
 
