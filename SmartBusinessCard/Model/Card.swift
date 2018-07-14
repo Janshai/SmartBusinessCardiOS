0d460e52ff36f802891211ef4851cd953619af19
Janshai <jack.r.s.adams@gmail.com>
2018-07-14 16:06:07 +0100

    gitignore

---
+++ SmartBusinessCard/Model/Card.swift
@@ -0,0 +1,16 @@
+//
+//  CardPresentation.swift
+//  SmartBusinessCard
+//
+//  Created by Jack Adams on 14/07/2018.
+//  Copyright © 2018 Jack Adams. All rights reserved.
+//
+
+import Foundation
+
+class Card {
+    var cardImageName: String?
+    var name: String
+    var website: URL?
+    var address: String?
+}
---
+++ SmartBusinessCard/Model/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  SmartBusinessCard
+//
+//  Created by Jack Adams on 14/07/2018.
+//  Copyright © 2018 Jack Adams. All rights reserved.
+//
+
+import Foundation
